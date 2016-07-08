1fd1febf9b30af0b4129bd996c5cd21f031c48b9
Max Beatty <max@beatty.me>
2016-07-07 20:37:31 -0700

    a simple web app with a database

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # example-config-evolution
 Example of a web app configuration evolving
 
-## v1
+## [v1](https://github.com/maxbeatty/example-config-evolution/tree/v1)
 
 It all starts with a simple web app that accepts requests and responds. It stands alone with no external dependencies or services. The only thing you might want to change is the port the server listens on.
+
+## [v2](https://github.com/maxbeatty/example-config-evolution/tree/v2)
+
+Now you want a persistent store such as a database so you add [PostgreSQL](https://www.postgresql.org/). You installed and started PostgreSQL locally and everything works fine. No configuration needed!
--- index.js
+++ index.js
@@ -1,9 +1,40 @@
 const http = require('http')
+const pg = require('pg')
 
 const server = http.createServer()
 
-server.on('request', function (req, res) {
-  res.end('ok')
+// the client will read connection information from
+// the same environment varaibles used by postgres cli tools
+const client = new pg.Client()
+
+server.on('request', (req, res) => {
+  new Promise((resolve, reject) => {
+    client.connect((err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
+  .then(() => {
+    return new Promise((resolve, reject) => {
+      client.query('SELECT 1;', (err, result) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(JSON.stringify(result))
+        }
+      })
+    })
+  })
+  .then((body) => {
+    res.end(body)
+  })
+  .catch((err) => {
+    // res.statusCode = 500
+    res.end(err.message)
+  })
 })
 
 server.listen(3000)
